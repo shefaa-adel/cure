c57336dfb250bfafb0c97e7d391692d2c0a15469
shefaa <47628748+shefaa-adel@users.noreply.github.com>
2020-10-14 23:46:25 +0200

    responsive

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
-var cure = document.querySelector(".nav__icon");
+var icon = document.querySelector(".nav__dropdown");
 var ul = document.querySelector(".nav__ul");
 var end = document.querySelector(".nav__end");
+// var cure = document.querySelector(".nav__icon");
 var open = false;
-var style=getComputedStyle(cure);
-var center=style.textAlign;
-// console.log(width);
+// var style = getComputedStyle(icon);
+// var visible = style.visibility;
+// console.log(visible);
 
 function cureClick() {
-    if (center === "center") {
-       
+    
+
         if (open == false) {
             ul.style.display = "block";
             end.style.display = "block";
+           
+            icon.classList.remove("fa-chevron-circle-down");
+            icon.classList.add("fa-chevron-circle-up");
             open = true;
         }
         else {
             ul.style.display = "none";
             end.style.display = "none";
+           
+            icon.classList.remove("fa-chevron-circle-up");
+            icon.classList.add("fa-chevron-circle-down");
             open = false;
         }
-    }
+    
 
 }
-cure.addEventListener("click", cureClick);
+icon.addEventListener("click", cureClick);
 
-
+console.log(pageYOffset);
 // stickynav
-var navbar=document.querySelector(".nav");
+var navbar = document.querySelector(".nav");
 
-window.onscroll=function(){
-    if(window.pageYOffset > 81){
+window.onscroll = function () {
+    if (window.pageYOffset > 80) {
         this.navbar.classList.add("nav--sticky");
 
     }
-    else{
+    else {
         this.navbar.classList.remove("nav--sticky");
     }
 };
